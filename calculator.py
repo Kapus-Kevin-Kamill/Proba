dde14f7b7d323df9226ee8a694966ad1ad7c6d27
Kapus Kevin Kamill <kapus.kevin.kamill@diak.szbi-pg.hu>
2024-10-02 11:21:49 +0200

    Add calculator file

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def calculate_sum(a, b):
+    return a + b
+
+def calculate_substraction(a, b):
+    return a - b
+
+def calculate_multiplication(a, b):
+    return a * b
+
+def calculate_devision(a, b):
+    return a / b
